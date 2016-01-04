41cb93c6debede5aa21e2c6d263a135d5a699562
cclient <cclient@hotmail.com>
2016-01-04 17:44:30 +0800

    update

--- webapp.py
+++ webapp.py
@@ -1,11 +1,9 @@
 # coding=utf-8
 from flask import Flask
 from api.v1.pcaip.controller import pcaroutemanager
-from api.v1.ap.controller import aproutemanager
 
 app = Flask(__name__)
 app.register_blueprint(pcaroutemanager)
-app.register_blueprint(aproutemanager)
 
 if __name__ == "__main__":
     app.debug = False
